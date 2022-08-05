84b6559b527c50e893d0bc8d80eeba75e8728e78
wgbn <jebinha@gmail.com>
2022-08-05 15:45:55 +0100

    2 files modified, 6 added, 1 deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksController } from './books.controller';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, BooksController],
+  imports: [ProductModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
